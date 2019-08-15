828b2e20db26c85a1e8fcc1536aab95898a96c9d
lihui <lihui@hst.com>
2019-08-15 19:25:32 +0800

    添加注释

--- DataStructAndAlgorithm/MyHeap.h
+++ DataStructAndAlgorithm/MyHeap.h
@@ -4,6 +4,9 @@
 #include "MyVector.h"
 using namespace std;
 
+/*
+最小堆 提供了插入和弹出最小元素
+*/
 template<typename T>
 class MyHeap
 {
@@ -37,33 +40,34 @@ public:
 
 	bool insert(T t)
 	{
-		int tempPos = ++curSize;
-		if (tempPos != 1)
+		int tempPos = ++curSize; // 堆大小增加 且为当前节点
+		if (tempPos != 1) //不是根元素插入
 		{
-			while (comparator(t, (*data)[tempPos / 2]) < 0)
+			while (comparator(t, (*data)[tempPos / 2]) < 0) //比较最后元素和他的父亲节点的大小 如果小于
 			{
-				(*data)[tempPos] = (*data)[tempPos / 2];
-				tempPos /= 2;
-				if (tempPos == 1)
+				(*data)[tempPos] = (*data)[tempPos / 2]; // 父亲节点往下移 
+				tempPos /= 2; //当前节点移到父亲节点
+				if (tempPos == 1) // 如果已是根节点 跳出循环
 				{
 					break;
 				}
 			}
 		}
 		
-		(*data)[tempPos] = t;
+		(*data)[tempPos] = t; //当前节点保存插入的元素
 		return true;
 	}
 
 	T  popMin()
 	{
-		T  result = (*data)[1];
-		T  last = (*data)[curSize];
-		int tempIndex = 1;
+		T  result = (*data)[1]; //保存结果
+		T  last = (*data)[curSize]; // 由于删除元素 需要为最后元素找合适位置 所以保存起来
+		int tempIndex = 1; 
 		int tempIndex2 = 1;
 		T  temp = (*data)[1];
-		
-		while (comparator(temp,last) < 0)
+
+		//比较当前节点和最后节点的大小 如果当前节点小于最后节点 则当前节点信息上移 直到找到合适位置
+		while (comparator(temp,last) < 0) 
 		{
 			if (2 * tempIndex > curSize)
 			{
